e9478dcce2d118a79037650e08a0f57b6dbc20eb
Victor Konstantinov <eclipse.vk@gmail.com>
2024-11-07 14:33:38 +0200

    Small improvements to the UI

--- ProjectAirsoft.Common/ApplicationConstants.cs
+++ ProjectAirsoft.Common/ApplicationConstants.cs
@@ -1,7 +1,9 @@
 ﻿namespace ProjectAirsoft.Common
 {
-	public static class ApplicationConstants
-	{
-		public const string CustomDateFormat = "dd.MM.yyyy";
-	}
+    public static class ApplicationConstants
+    {
+        public const string CustomDateFormat = "dd.MM.yyyy";
+        public const string DefaultGameImage = "/images/default-game-image.jpg";
+        public const int ReleaseYear = 2024;
+    }
 }
--- ProjectAirsoft.Services.Data/GameService.cs
+++ ProjectAirsoft.Services.Data/GameService.cs
@@ -8,55 +8,55 @@ using static ProjectAirsoft.Common.ApplicationConstants;
 
 namespace ProjectAirsoft.Services.Data
 {
-	public class GameService(ApplicationDbContext dbContext) : IGameService
-	{
-		public async Task<IEnumerable<GameIndexViewModel>> GetAllGamesAsync()
-		{
-			List<Game> games = await dbContext.Games
-				.Include(g => g.Terrain)
-				.ToListAsync();
+    public class GameService(ApplicationDbContext dbContext) : IGameService
+    {
+        public async Task<IEnumerable<GameIndexViewModel>> GetAllGamesAsync()
+        {
+            List<Game> games = await dbContext.Games
+                .Include(g => g.Terrain)
+                .ToListAsync();
 
-			IEnumerable<GameIndexViewModel> gameIndexViewModels = games
-				.Where(g => g.IsDeleted == false)
-				.OrderBy(g => g.Date)
-				.Select(g => new GameIndexViewModel()
-				{
-					Id = g.Id.ToString(),
-					Name = g.Name,
-					ImageUrl = g.ImageUrl != null ? g.ImageUrl : "/images/airsoft-banner-medium.jpg",
-					Date = g.Date.ToString(CustomDateFormat),
-					Terrain = g.Terrain.Name
-				});
+            IEnumerable<GameIndexViewModel> gameIndexViewModels = games
+                .Where(g => g.IsDeleted == false)
+                .OrderBy(g => g.Date)
+                .Select(g => new GameIndexViewModel()
+                {
+                    Id = g.Id.ToString(),
+                    Name = g.Name,
+                    ImageUrl = g.ImageUrl != null ? g.ImageUrl : DefaultGameImage,
+                    Date = g.Date.ToString(CustomDateFormat),
+                    Terrain = g.Terrain.Name
+                });
 
-			return gameIndexViewModels;
-		}
+            return gameIndexViewModels;
+        }
 
-		public async Task<bool> AddGameAsync(GameCreateViewModel viewModel, string userId)
-		{
-			bool isDateValid = DateTime.TryParse(viewModel.Date, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate);
+        public async Task<bool> AddGameAsync(GameCreateViewModel viewModel, string userId)
+        {
+            bool isDateValid = DateTime.TryParse(viewModel.Date, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate);
 
-			if (!isDateValid)
-			{
-				return false;
-			}
+            if (!isDateValid)
+            {
+                return false;
+            }
 
-			var game = new Game()
-			{
-				Name = viewModel.Name,
-				Description = viewModel.Description,
-				ImageUrl = viewModel.ImageUrl,
-				Date = parsedDate,
-				StartTime = viewModel.StartTime,
-				Capacity = viewModel.Capacity,
-				Fee = viewModel.Fee,
-				TerrainId = Guid.Parse(viewModel.TerrainId),
-				OrganizerId = Guid.Parse(userId)
-			};
+            var game = new Game()
+            {
+                Name = viewModel.Name,
+                Description = viewModel.Description,
+                ImageUrl = viewModel.ImageUrl,
+                Date = parsedDate,
+                StartTime = viewModel.StartTime,
+                Capacity = viewModel.Capacity,
+                Fee = viewModel.Fee,
+                TerrainId = Guid.Parse(viewModel.TerrainId),
+                OrganizerId = Guid.Parse(userId)
+            };
 
-			await dbContext.Games.AddAsync(game);
-			await dbContext.SaveChangesAsync();
+            await dbContext.Games.AddAsync(game);
+            await dbContext.SaveChangesAsync();
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }

bb2f28a84aa79c80891d4ec5c765a83139ee1306
Victor Konstantinov <eclipse.vk@gmail.com>
2024-11-26 18:06:24 +0200

    Minor edit to validation messages

--- ProjectAirsoft.Common/ValidationMessages.cs
+++ ProjectAirsoft.Common/ValidationMessages.cs
@@ -4,65 +4,65 @@
 	{
 		public static class Game
 		{
-			public const string RequiredNameMessage = "The Name is required.";
-			public const string NameMinLengthMessage = "The Name length must be at least {1} characters.";
-			public const string NameMaxLengthMessage = "The Name length cannot be more than {1} characters.";
+			public const string RequiredNameMessage = "The name is required.";
+			public const string NameMinLengthMessage = "The name length must be at least {1} characters.";
+			public const string NameMaxLengthMessage = "The name length cannot be more than {1} characters.";
 
-			public const string RequiredDescriptionMessage = "The Description is required.";
-			public const string DescriptionMinLengthMessage = "The Description length must be at least {1} characters.";
-			public const string DescriptionMaxLengthMessage = "The Description length cannot be more than {1} characters.";
+			public const string RequiredDescriptionMessage = "The description is required.";
+			public const string DescriptionMinLengthMessage = "The description length must be at least {1} characters.";
+			public const string DescriptionMaxLengthMessage = "The description length cannot be more than {1} characters.";
 
-			public const string ImageUrlMaxLengthMessage = "The Image Link length cannot be more than {1} characters.";
+			public const string ImageUrlMaxLengthMessage = "The image link length cannot be more than {1} characters.";
 
-			public const string RequiredDateMessage = "The Date is required.";
+			public const string RequiredDateMessage = "The date is required.";
 
-			public const string RequiredStartTimeMessage = "The Start Time is required.";
-			public const string InvalidStartTimeMessage = "The Start Time must be in the following format: HH:mm";
+			public const string RequiredStartTimeMessage = "The start time is required.";
+			public const string InvalidStartTimeMessage = "The start time must be in the following format: HH:mm";
 
-			public const string RequiredCapacityMessage = "The Capacity is required.";
-			public const string CapacityRangeMessage = "The Capacity must be between {1} and {2}.";
-			public const string CapacityLessThanRegisteredPlayers = "The Capacity cannot be less than the current registered players.";
+			public const string RequiredCapacityMessage = "The capacity is required.";
+			public const string CapacityRangeMessage = "The capacity must be between {1} and {2}.";
+			public const string CapacityLessThanRegisteredPlayers = "The capacity cannot be less than the current registered players.";
 
-			public const string RequiredFeeMessage = "The Fee is required.";
-			public const string FeeRangeMessage = "The Fee must be between {1} and {2}.";
+			public const string RequiredFeeMessage = "The fee is required.";
+			public const string FeeRangeMessage = "The fee must be between {1} and {2}.";
 
-			public const string RequiredTerrainMessage = "The Terrain is required.";
+			public const string RequiredTerrainMessage = "The terrain is required.";
 		}
 
 		public static class Terrain
 		{
-			public const string RequiredNameMessage = "The Name is required.";
-			public const string NameMinLengthMessage = "The Name length must be at least {1} characters.";
-			public const string NameMaxLengthMessage = "The Name length cannot be more than {1} characters.";
+			public const string RequiredNameMessage = "The name is required.";
+			public const string NameMinLengthMessage = "The name length must be at least {1} characters.";
+			public const string NameMaxLengthMessage = "The name length cannot be more than {1} characters.";
 
-			public const string RequiredLocationMessage = "The Location is required.";
-			public const string LocationUrlMaxLengthMessage = "The Location Link length cannot be more than {1} characters.";
+			public const string RequiredLocationMessage = "The location is required.";
+			public const string LocationUrlMaxLengthMessage = "The location link length cannot be more than {1} characters.";
 
-			public const string RequiredCityMessage = "The City is required.";
+			public const string RequiredCityMessage = "The city is required.";
 		}
 
 		public static class Comment
 		{
-			public const string RequiredContentMessage = "The Comment cannot be empty.";
-			public const string ContentMinLengthMessage = "The Comment length must be at least {1} characters.";
-			public const string ContentMaxLengthMessage = "The Comment length cannot be more than {1} characters.";
+			public const string RequiredContentMessage = "The comment cannot be empty.";
+			public const string ContentMinLengthMessage = "The comment length must be at least {1} characters.";
+			public const string ContentMaxLengthMessage = "The comment length cannot be more than {1} characters.";
 		}
 
 		public static class Team
 		{
-			public const string RequiredNameMessage = "The Name is required.";
-			public const string NameMinLengthMessage = "The Name length must be at least {1} characters.";
-			public const string NameMaxLengthMessage = "The Name length cannot be more than {1} characters.";
+			public const string RequiredNameMessage = "The name is required.";
+			public const string NameMinLengthMessage = "The name length must be at least {1} characters.";
+			public const string NameMaxLengthMessage = "The name length cannot be more than {1} characters.";
 
-			public const string RequiredLogoMessage = "The Logo is required.";
-			public const string LogoUrlMaxLengthMessage = "The Logo Link length cannot be more than {1} characters.";
+			public const string RequiredLogoMessage = "The logo is required.";
+			public const string LogoUrlMaxLengthMessage = "The logo link length cannot be more than {1} characters.";
 
-			public const string RequiredCityMessage = "The City is required.";
+			public const string RequiredCityMessage = "The city is required.";
 		}
 
 		public static class FutureDateAttribute
 		{
-			public const string RequiredDateMessage = "The Date is required.";
+			public const string RequiredDateMessage = "The date is required.";
 			public const string PastDateMessage = "The selected date cannot be in the past.";
 			public const string InvalidDateMessage = "Invalid date format.";
 		}

8918194ac54d57f9611d9e4ef7960ff8e134e08c
Victor Konstantinov <eclipse.vk@gmail.com>
2024-12-01 14:27:08 +0200

    Forbid deleted users from logging in

--- ProjectAirsoft.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProjectAirsoft.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -108,6 +108,14 @@ namespace ProjectAirsoft.Web.Areas.Identity.Pages.Account
 
 			if (ModelState.IsValid)
 			{
+				ApplicationUser user = await _userManager.FindByNameAsync(Input.Username);
+
+				if (user != null && user.IsDeleted)
+				{
+					ModelState.AddModelError(string.Empty, "This user has been deleted. For more information please contact the system administrator.");
+					return Page();
+				}
+
 				// This doesn't count login failures towards account lockout
 				// To enable password failures to trigger account lockout, set lockoutOnFailure: true
 				var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
